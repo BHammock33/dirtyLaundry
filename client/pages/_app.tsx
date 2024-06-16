26256696190ee2d81791f310290bea91760aace2
BHammock33 <BennettHammock@gmail.com>
2024-06-16 17:40:05 -0400

    Sockets up and running

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
 import SocketsProvider from "../context/socket.context";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SocketsProvider>
       <Component {...pageProps} />
--- dlServer/src/app.ts
+++ dlServer/src/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import config from "config";
 import log from "./utils/logger";
 import { version } from '../package.json';
+import socket from "./socket";
 
 const port = config.get<number>("port");
 const host = config.get<string>("host");
@@ -26,4 +27,6 @@ app.get('/', (_, res) => res.send(`Server is up and running version ${version}`)
 httpServer.listen(port, host, () =>{
     log.info(`Server version ${version} is listening`);
     log.info(`http://${host}:${port}`);
+
+    socket({io});
 });
